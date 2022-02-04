cf62867b4b80f8e3962d35e84ecfecde57be735e
Mai Abou Elmagd <47284621+Mai-abouelmagd@users.noreply.github.com>
2022-02-05 00:13:40 +0200

    Add files via upload

---
+++ sign up/JS/main.js
@@ -0,0 +1,8 @@
+const btn = document.querySelector('.btn');
+btn.addEventListener('click', (e) => {
+    e.preventDefault();
+    document.querySelector('#my-form').style.background ='#ccc';
+    document.querySelector('body').classList.add('bg-dark');
+
+
+});
